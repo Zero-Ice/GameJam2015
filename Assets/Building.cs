0602c241bc8d5b13e9fe5228f15b40a6cd215e7d
RuiPeng <ongruipeng@gmail.com>
2015-12-11 13:23:03 +0800

    Initial commit

---
+++ Assets/Building.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Building : MonoBehaviour {
+
+	public string buildingName;
+	public int buildingIndex;
+	public float stockPrice;
+	public float finalStockPrice;
+	public float multiplier;
+
+	List<Debt> debtors;
+
+
+	// Use this for initialization
+	void Start () {
+		debtors = new List<Debt>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void AddDebtor(Player otherPlayer) {
+		debtors.Add(new Debt(otherPlayer));
+	}
+
+	public void UpdateDebtors() {
+		foreach (Debt x in debtors) {
+			if(!x.UpdateTurn()){
+				// If player has enough stock to return to the building
+				if(x.player.stockDataList[buildingIndex].stocksBorrowed >= x.noStocksOwed) {
+					x.player.stockDataList[buildingIndex].stocksBorrowed -= x.noStocksOwed;
+					ClearDebtor(x);
+				} else {
+					// If player fails to pay in stocks, pay money
+					x.player.money -= x.noStocksOwed * stockPrice;
+				}
+			}
+		}
+	}
+
+	public void ClearDebtor(Player otherPlayer) {
+		foreach (Debt x in debtors) {
+			if(x.player == otherPlayer) {
+				ClearDebtor(x);
+				break;
+			}
+		}
+	}
+
+	void ClearDebtor(Debt otherDebt) {
+		debtors.Remove (otherDebt);
+	}
+}
+
+public class Debt : MonoBehaviour {
+	
+	public Player player;
+	public float noStocksOwed;
+	byte turns;
+	
+	public Debt(Player otherPlayer) {
+		turns = 5;
+		this.player = otherPlayer;
+	}
+	
+	public bool UpdateTurn() {
+		--turns;
+		if (turns > 0)
+			return true;
+		else {
+			return false;
+		}
+	}
+	
+	void Start() {
+		
+	}
+	
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Player : MonoBehaviour {
+
+	public List<StockData> stockDataList;
+
+	public float money;
+
+	public bool turn, done;
+
+	// Use this for initialization
+	void Start () {
+		money = 200000;
+		turn = false;
+		done = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public void UpdateTurn() {
+
+	}
+
+	public void BuyStock(int buildingIndex, Building otherBuilding) {
+		stockDataList [buildingIndex].stocksBought += 1000;
+	}
+
+	public void BorrowStock(int buildingIndex, Building otherBuilding){
+		otherBuilding.AddDebtor (this);
+		stockDataList [buildingIndex].stocksBorrowed += 1000;
+	}
+
+	public void ReturnStock(int buildingIndex, Building otherBuilding) {
+		otherBuilding.ClearDebtor (this);
+		stockDataList [buildingIndex].stocksBorrowed -= 1000;
+	}
+}
+
+public class StockData : MonoBehaviour {
+
+	public Building building;
+	// Stocks bought from that company
+	public float stocksBought;
+	// Stocks borrowed from that company
+	public float stocksBorrowed;
+}
+
+
