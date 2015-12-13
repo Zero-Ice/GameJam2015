0af3061febde46a1a9409538e9d1d78368ede1cf
hydrater <liquid_assassin@hotmail.com>
2015-12-13 13:08:32 -0800

    Dice improved

--- Assets/Scripts/Dice.cs
+++ Assets/Scripts/Dice.cs
@@ -24,70 +24,41 @@ public class Dice : MonoBehaviour {
 		{
 			Vector3 r = transform.eulerAngles;
 
-			if (timer >= -1)
+			if (timer <= -1)
 			{
 				switch(diceResult)
 				{
 					case 1:
-					transform.rotation = Quaternion.Euler(Mathf.LerpAngle(r.x, 90, Time.deltaTime * 40), r.y, Mathf.LerpAngle(r.z, 0, Time.deltaTime * 40));
+					transform.rotation = Quaternion.Euler(Mathf.LerpAngle(r.x, 90, Time.deltaTime * 10), r.y, Mathf.LerpAngle(r.z, 0, Time.deltaTime * 10));
 					break;
 
 					case 2:
-					transform.rotation = Quaternion.Euler(Mathf.LerpAngle(r.x, 0, Time.deltaTime * 40), r.y, Mathf.LerpAngle(r.z, 270, Time.deltaTime * 40));
+					transform.rotation = Quaternion.Euler(Mathf.LerpAngle(r.x, 0, Time.deltaTime * 10), r.y, Mathf.LerpAngle(r.z, 270, Time.deltaTime * 10));
 					break;
 
 					case 3:
-					transform.rotation = Quaternion.Euler(Mathf.LerpAngle(r.x, 0, Time.deltaTime * 40), r.y, Mathf.LerpAngle(r.z, 0, Time.deltaTime * 40));
+					transform.rotation = Quaternion.Euler(Mathf.LerpAngle(r.x, 0, Time.deltaTime * 10), r.y, Mathf.LerpAngle(r.z, 0, Time.deltaTime * 10));
 					break;
 
 					case 4:
-					transform.rotation = Quaternion.Euler(Mathf.LerpAngle(r.x, 0, Time.deltaTime * 40), r.y, Mathf.LerpAngle(r.z, 180, Time.deltaTime * 40));
+					transform.rotation = Quaternion.Euler(Mathf.LerpAngle(r.x, 0, Time.deltaTime * 10), r.y, Mathf.LerpAngle(r.z, 180, Time.deltaTime * 10));
 					break;
 
 					case 5:
-					transform.rotation = Quaternion.Euler(Mathf.LerpAngle(r.x, 0, Time.deltaTime * 40), r.y, Mathf.LerpAngle(r.z, 90, Time.deltaTime * 40));
+					transform.rotation = Quaternion.Euler(Mathf.LerpAngle(r.x, 0, Time.deltaTime * 10), r.y, Mathf.LerpAngle(r.z, 90, Time.deltaTime * 10));
 					break;
 				
 					case 6:
-					transform.rotation = Quaternion.Euler(Mathf.LerpAngle(r.x, -90, Time.deltaTime * 40), r.y, r.z);
+					transform.rotation = Quaternion.Euler(Mathf.LerpAngle(r.x, -90, Time.deltaTime * 10), r.y, r.z);
 					break;
 				 }
-			}
-			else
-			{
-				switch(diceResult)
-				{
-				case 1:
-					transform.rotation = Quaternion.Euler(90, r.y, 0);
-					break;
-					
-				case 2:
-					transform.rotation = Quaternion.Euler(0, r.y, 270);
-					break;
-					
-				case 3:
-					transform.rotation = Quaternion.Euler(0, r.y, 0);
-					break;
-					
-				case 4:
-					transform.rotation = Quaternion.Euler(0, r.y, 180);
-					break;
-					
-				case 5:
-					transform.rotation = Quaternion.Euler(0, r.y, 90);
-					break;
-					
-				case 6:
-					transform.rotation = Quaternion.Euler(-90, r.y, r.z);
-					break;
-				}
 			}
 
 			if (timer <= -3)
 			{
-//				GameObject.Find("Game Manager").GetComponent<GameManager>().canMove = true;
-//				GameObject.Find("Game Manager").GetComponent<GameManager>().diceRolling = false;
-//				Destroy(this.gameObject);
+				GameObject.Find("Game Manager").GetComponent<GameManager>().canMove = true;
+				GameObject.Find("Game Manager").GetComponent<GameManager>().diceRolling = false;
+				Destroy(this.gameObject);
 			}
 		}
 	}
